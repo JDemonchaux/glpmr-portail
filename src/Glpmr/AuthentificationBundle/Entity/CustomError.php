e15749a6c4dfe4f445b42e4e51a3f81ee594dca8
shorty-fr <jerome.demonchaux@gmail.com>
2015-11-21 11:22:21 +0100

    final merged work

---
+++ src/Glpmr/AuthentificationBundle/Entity/CustomError.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: jerom
+ * Date: 18/09/2015
+ * Time: 11:23
+ */
+
+namespace Glpmr\AuthentificationBundle\Entity;
+
+
+class CustomError
+{
+
+}
---
+++ src/Glpmr/PeripheriqueBundle/Tests/PeripheriqueTest.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: jerom
+ * Date: 23/09/2015
+ * Time: 11:39
+ */
+class PeripheriqueTest extends PHPUnit_Framework_TestCase
+{
+
+}
