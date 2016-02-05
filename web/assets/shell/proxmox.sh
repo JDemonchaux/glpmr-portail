e3a1064a3dafefcb78f136b1d1e39289e1589950
skrface <derbitrash@htomail.fr>
2016-02-05 10:33:03 +0100

    tralala

--- web/assets/shell/proxmox.sh
+++ web/assets/shell/proxmox.sh
@@ -4,53 +4,48 @@
 #Connexion en ssh au serveur proxmox 172.31.10.1
 
 
-
 osTemplate = $1
-ipAdresse = $2
-hostname = $3
-password = $4
+ip = $2
+nomMachine = $3
+mdp = $4
 id = $5
 lastOctets = $6
 
+
+read -p 'Saisir un port pour lacces exterieur: ' port
+
 #Création de la vm sur le proxmox
-ssh root@172.31.10.1 vzctl create $id --ostemplate $osTemplate #cr�ation du conteneur avec un num�ro et choix du template
-ssh root@172.31.10.1 vzctl set $id --onboot yes --ipadd $ipAdresse --nameserver 172.31.1.1 8.8.8.8 --hostname $hostname.reseau-labo.fr --searchdomain reseau-labo.fr --cpus 1 --ram 512M --swap 512M --diskspace 20G --save #configuration du conteneur
-ssh root@172.31.10.1 vzctl set $id --userpasswd $hostname:$password --save
-ssh root@172.31.10.1 vzctl start $id
-echo "La machine a ete cree avec succee"
-
-
-
-#Cr�ation de la vm sur le proxmox
-#ssh root@172.31.10.1 vzctl create $ --ostemplate debian-7.0-standard_7.0-2_i386 #cr�ation du conteneur avec un num�ro et choix du template
-#ssh root@172.31.10.1 vzctl set 102 --onboot yes --ipadd 172.31.10.50 --nameserver 172.31.1.1 8.8.8.8 --hostname testYoucef.reseau-labo.fr --searchdomain reseau-labo.fr --cpus 1 --ram 512M --swap 512M --diskspace 20G --save #configuration du conteneur
-#ssh root@172.31.10.1 vzctl set 102 --userpasswd root:root --save
-#ssh root@172.31.10.1 vzctl start 102
-#echo "La machine a ete cree avec succee"
+ssh root@172.31.10.1 vzctl create "$lastOctets" --ostemplate debian-7.0-standard_7.0-2_i386 #création du conteneur avec un numéro et choix du template
+ssh root@172.31.10.1 vzctl set "$lastOctets" --onboot yes --ipadd "$lastOctets" --nameserver 172.31.1.1 --hostname "$nomMachine".reseau-labo.fr --searchdomain reseau-labo.fr --cpus 1 --ram 512M --swap 512M --diskspace 20G --save #configuration du conteneur
+ssh root@172.31.10.1 vzctl set "$lastOctets" --userpasswd root:"$mdp" --save #création du compte utilisateur avec mdp
+ssh root@172.31.10.1 vzctl start "$lastOctets" #demarage du conteneur
+echo "La machine $nomMachine a ete cree avec succee"
 
 #Connexion en ssh au serveur DNS 172.31.1.1
 #Ajout au DNS DMZ
-echo '$hostname         IN	CNAME	labo.wha.la.' | ssh root@172.31.1.1 "cat >> /etc/bind/db/db.reseau-labo.fr"
-echo '$lastOctets	IN	PTR	$hostname.reseau-labo.fr' | ssh root@172.31.1.1 "cat >> /etc/bind/db/db.31.172.in-addr.arpa"
-ssh root@172.31.1.1 /etc/init.d/bind9 restart
+ssh root@172.31.1.1 echo "$nomMachine	IN	CNAME	labo.wha.la." ' >> /etc/bind/db/db.reseau-labo.fr' #recuperation du nom du conteneur
+ssh root@172.31.1.1 echo "$lastOctets  IN	PTR	$nomMachine.reseau-labo.fr." ' >> /etc/bind/db/db.31.172.in-addr.arpa' #recuperation du nom du conteneur ainsi que la fin de l'adresse ip
+ssh root@172.31.1.1 /etc/bind/scripts/incrSerial.sh #Lancement du script pour incrementer serial
+ssh root@172.31.1.1 /etc/init.d/bind9 restart #redemarage du service bind
 
-echo "La machine a ete ajouter au serveur DNS"
+echo "La machine $nomMachine a ete ajouter au serveur DNS"
 
 #Creation de la regle Nat
-ssh root@172.31.0.1 'iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 22222 -j DNAT --to-destination $ipAdresse:22'
+ssh root@172.31.0.1 "iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 22222 -j DNAT --to-destination $ip:22"  #recuperation de l'adresse ip de la machine
 
 #Creation virtualhost
-echo '<VirtualHost *:80>
-	ServerName $hostname.reseau-labo.fr
-	ProxyPreserveHost On
-	ProxyRequests On
-	ProxyPass / http://$ipAdresse/
-	ProxyPassReverse / http://$ipAdresse/
-</VirtualHost>' | ssh root@172.31.0.1 "cat > /etc/apache2/sites-available/$hostname.reseau-labo.fr"
+ssh root@172.31.0.1 "(echo '<VirtualHost *:80>'
+echo ServerName $nomMachine.reseau-labo.fr 
+echo ProxyPreserveHost On
+echo ProxyRequests On
+echo ProxyPass / http://$ip/
+echo ProxyPassReverse / http://$ip/
+echo '</VirtualHost>')" " > /etc/apache2/sites-available/$nomMachine.reseau-labo.fr"  #recuperation du nom du conteneur et son adresse ip 
 
 echo "la virtuahost a ete cree avec succee"
 
-ssh root@172.31.0.1 a2ensite $hostname.reseau-labo.fr
-ssh root@172.31.0.1 service apache2 reload
+ssh root@172.31.0.1 "a2ensite $nomMachine.reseau-labo.fr" #activation du virtualhost
+ssh root@172.31.0.1 service apache2 reload #redemarage du service apache2
 
 echo "Le serveur apache a ete redemarrer avec succee"
+echo "la virtuahost a ete cree avec succee"
